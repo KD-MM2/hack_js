15c197bb6a404a1acea86c845eb6e38ff62f7344
Jamch <34964717+jamch@users.noreply.github.com>
2020-07-08 01:31:13 +0800

    Update clarity.js

--- clarity.js
+++ clarity.js
@@ -6,27 +6,29 @@ const userinfo = '/userInfo';
 
 const setfree = '/special/queryByCatalogAll';
 
-const setfreeitem = '/special'
-
 if (url.indexOf(setfree) != -1) {
-   for (var i = 0; i < obj.data.length; i++) {
-       obj.data[i].isFree = true;
-   }
-   body = JSON.stringify(obj);
+    for (var i = 0; i < obj.data.length; i++) {
+        obj.data[i].isFree = true;
+    }
+    body = JSON.stringify(obj);
 }
 
-if (url.indexOf(setfreeitem) != -1) {
-   obj.data.isFree = true;
-   for (var i = 0; i < obj.data.pictureList.length; i++) {
-       obj.data.pictureList[i].isFree = true;
-   }
-   body = JSON.stringify(obj);
+if (!obj.data.isFree) {
+    obj.data.isFree = true;
+    if (obj.data.pictureList) {
+        for (var i = 0; i < obj.data.pictureList.length; i++) {
+            obj.data.pictureList[i].isFree = true;
+        }
+        body = JSON.stringify(obj);
+    }
 }
 
 if (url.indexOf(userinfo) != -1) {
-   obj.data.level = 5;
-   obj.data.expireTime = 4070965662;
-   body = JSON.stringify(obj);
+    obj.data.level = 5;
+    obj.data.expireTime = 4070965662;
+    body = JSON.stringify(obj);
 }
 
-$done({body});
+$done({
+    body
+});
