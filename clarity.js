5fd6696a3fe4cea0677a30cc65611688dc9e8ea5
Jamch <34964717+jamch@users.noreply.github.com>
2020-07-08 01:35:57 +0800

    Update clarity.js

--- clarity.js
+++ clarity.js
@@ -7,28 +7,16 @@ const userinfo = '/userInfo';
 const setfree = '/special/queryByCatalogAll';
 
 if (url.indexOf(setfree) != -1) {
-    for (var i = 0; i < obj.data.length; i++) {
-        obj.data[i].isFree = true;
-    }
-    body = JSON.stringify(obj);
-}
-
-if (!obj.data.isFree) {
-    obj.data.isFree = true;
-    if (obj.data.pictureList) {
-        for (var i = 0; i < obj.data.pictureList.length; i++) {
-            obj.data.pictureList[i].isFree = true;
-        }
-        body = JSON.stringify(obj);
-    }
+   for (var i = 0; i < obj.data.length; i++) {
+       obj.data[i].isFree = true;
+   }
+   body = JSON.stringify(obj);
 }
 
 if (url.indexOf(userinfo) != -1) {
-    obj.data.level = 5;
-    obj.data.expireTime = 4070965662;
-    body = JSON.stringify(obj);
+   obj.data.level = 5;
+   obj.data.expireTime = 4070965662;
+   body = JSON.stringify(obj);
 }
 
-$done({
-    body
-});
+$done({body});
